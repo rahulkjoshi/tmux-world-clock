772dfb14839f5095cdd4e386a6ad9987d4d581f9
Rahul Joshi <rahul.joshi2012@gmail.com>
2024-03-30 02:36:37 -0400

    Initial commit

---
+++ scripts/world-clock.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+# vi: ft=bash
+
+function main() {
+    local tz_csv
+    IFS="," tz_csv=( $( tmux show -gqv "@world_clock_tzs" ) ); unset IFS;
+
+    local main_style=$( tmux show -gqv "@world_clock_main_style" )
+    local sec_style=$( tmux show -gqv "@world_clock_sec_style" )
+    local date_fmt=$( tmux show -gqv "@world_clock_date_fmt" )
+    if [[ -n "${date_fmt}" ]]; then
+        date_fmt=" ${date_fmt}"
+    fi
+    local time_fmt=$( tmux show -gqv "@world_clock_time_fmt" )
+
+    local is_first=true
+    for tz in "${tz_csv[@]}"; do
+        if [[ "$is_first" == true ]]; then
+            printf "${main_style}"
+        else
+            printf "#[fg=default] | ${sec_style}"
+        fi
+
+        local t=$( TZ="${tz}" date +"${time_fmt}${date_fmt}" )
+        printf "${t}"
+
+        if [[ "$is_first" == true ]]; then
+            is_first=false
+            date_fmt=""
+        fi
+    done
+    printf "#[fg=default]"
+}
+
+main
---
+++ tmux-world-clock.tmux
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+#! vi: ft=bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT="${CURRENT_DIR}/scripts/world-clock.sh"
+
+function main() {
+    local status_left="$( tmux show -gqv "status-left" | sed 's!#{world_clock}!#('"${SCRIPT}"')!' )"
+    tmux set -gq "status-left" "${status_left}"
+
+    local status_right="$( tmux show -gqv "status-right" | sed 's!#{world_clock}!#('"${SCRIPT}"')!' )"
+    tmux set -gq "status-right" "${status_right}"
+}
+
+main
