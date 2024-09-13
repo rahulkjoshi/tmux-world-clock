e1c045ae7b0f737a766d605ed43ac40d18471024
Rahul Joshi <rkjoshi@google.com>
2024-09-13 11:50:17 -0400

    Add date style and move date to end

--- scripts/world-clock.sh
+++ scripts/world-clock.sh
@@ -5,31 +5,32 @@ function main() {
     local tz_csv
     IFS="," tz_csv=( $( tmux show -gqv "@world_clock_tzs" ) ); unset IFS;
 
-    local main_style=$( tmux show -gqv "@world_clock_main_style" )
+    local default_style="#[fg=default bg=default]"
+
+    local pri_style=$( tmux show -gqv "@world_clock_pri_style" )
+    pri_style="${pri_style:-${default_style}}"
     local sec_style=$( tmux show -gqv "@world_clock_sec_style" )
+    sec_style="${sec_style:-${default_style}}"
+    local date_style=$( tmux show -gqv "@world_clock_date_style" )
+    date_style="${date_style:-${default_style}}"
+
     local date_fmt=$( tmux show -gqv "@world_clock_date_fmt" )
-    if [[ -n "${date_fmt}" ]]; then
-        date_fmt=" ${date_fmt}"
-    fi
     local time_fmt=$( tmux show -gqv "@world_clock_time_fmt" )
 
     local is_first=true
     for tz in "${tz_csv[@]}"; do
         if [[ "$is_first" == true ]]; then
-            printf "${main_style:-#[fg=default]}"
-        else
-            printf "#[fg=default] | ${sec_style:-#[fg=default]}"
-        fi
-
-        local t=$( TZ="${tz}" date +"${time_fmt}${date_fmt}" )
-        printf "${t}"
-
-        if [[ "$is_first" == true ]]; then
+            printf "${pri_style}"
             is_first=false
-            date_fmt=""
+        else
+            printf "#[fg=default] | ${sec_style}"
         fi
+
+        printf "$( TZ="${tz}" date +"${time_fmt}" )"
     done
-    printf "#[fg=default]"
+    printf  "#[fg=default] | ${date_style}"
+    printf "$( TZ="${tz}" date +"${date_fmt}" )"
+    printf "${default_style}"
 }
 
 main
